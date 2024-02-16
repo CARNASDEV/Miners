19fd7ceb742a771896bcde4fd68d7070da6c4929
Carolina Correia <94carcon@gafe.molndal.se>
2024-02-16 20:09:42 +0100

    Menu first draft

--- src/main/java/com/colab/minerscli/Main.java
+++ src/main/java/com/colab/minerscli/Main.java
@@ -2,6 +2,7 @@ package com.colab.minerscli;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Menu menu = new Menu();
+        Menu.showMenu();
     }
 }
